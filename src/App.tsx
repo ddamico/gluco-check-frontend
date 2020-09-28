5941ce3c7bd32ef22ab382cd556fea3b7363533e
David D'Amico <human@ddamico.org>
2020-09-27 21:19:46 -0400

    adds very very basic login support

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { auth } from "./lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Landing from "./pages/Landing";
+import EditSettings from "./pages/EditSettings";
 
-function App() {
+export default function App() {
+  const [user, loading] = useAuthState(auth);
+  let Content: React.ReactElement | null = null;
+  if (user) {
+    Content = <EditSettings user={user} />;
+  } else {
+    if (!loading) {
+      Content = <Landing />;
+    }
+  }
   return (
-    <div className="App">
-      Hey
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {!user && (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+          {user && (
+            <button
+              onClick={() => {
+                auth.signOut();
+              }}
+            >
+              Logout
+            </button>
+          )}
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            {Content}
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div>
+      <h2>Gluco Check</h2>
+    </div>
+  );
+}
+
+export default Landing;
