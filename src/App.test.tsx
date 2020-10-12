8fa40e5430be0a7d33b69ee72a3643dbb2cc30db
David D'Amico <human@ddamico.org>
2020-10-12 00:14:31 -0400

    fixing App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,30 +1,57 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
+import { auth } from "./lib/firebase";
 import * as firebaseAuthHooks from "react-firebase-hooks/auth";
 import App from "./App";
 import { mockUser } from "./lib/__mocks__/firebase"
+
+jest.mock("./lib/firebase.ts", () => {
+  return {
+    auth: jest.fn().mockReturnThis(),
+  };
+});
+
+jest.mock("./pages/EditSettings.tsx", () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return <div>EditSettings</div>;
+    },
+  };
+});
+
+jest.mock("./pages/Login.tsx", () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return <div>Login</div>;
+    },
+  };
+});
 
 expect.extend(toHaveNoViolations);
 
 afterEach(cleanup);
 
 describe("App component", () => {
-  test("renders the component when user loading", () => {
+  it("renders the component when user loading", () => {
     // @ts-ignore
     firebaseAuthHooks.useAuthState = jest.fn().mockReturnValue([undefined, true]);
     const { container } = render(<App />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  test("renders the component when user loaded", () => {
+  it("renders the component when user loaded", () => {
     // @ts-ignore
     firebaseAuthHooks.useAuthState = jest.fn().mockReturnValue([mockUser, false]);
     const { container } = render(<App />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  test("has no axe violations", async () => {
+  it("has no axe violations", async () => {
+    // @ts-ignore
+    firebaseAuthHooks.useAuthState = jest.fn().mockReturnValue([mockUser, false]);
     const { container } = render(<App />);
     expect(await axe(container)).toHaveNoViolations();
   });
