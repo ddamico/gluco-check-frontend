c7226e0d46f0615cc122577f88ade9cb1f7140d1
David D'Amico <human@ddamico.org>
2020-10-12 10:16:58 -0400

    unused import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup, prettyDOM } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
 import userEvent from "@testing-library/user-event";
 import { auth } from "./lib/firebase";
