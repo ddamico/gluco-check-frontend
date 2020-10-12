9ffed3250890c8e55ccacc97b8fbf03761386cb5
David D'Amico <human@ddamico.org>
2020-10-12 16:25:37 -0400

    removing todo

--- src/lib/__mocks__/firebase.ts
+++ src/lib/__mocks__/firebase.ts
@@ -1,7 +1,6 @@
 import { BloodGlucoseUnits } from "../enums"
 import { GlucoCheckUserDocument } from "../types"
 
-// @TODO: fill in the test of this
 export const mockUser: Partial<firebase.User> = {
     email: 'example@example.com',
     uid: "uid123"
