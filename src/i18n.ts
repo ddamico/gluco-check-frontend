cf5832ca02876879c210d102d55646e64dbf1815
David D'Amico <human@ddamico.org>
2020-10-18 14:14:43 -0400

    improving i18n setup

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 // import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
+    .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
 //   .use(LanguageDetector)
@@ -12,11 +14,10 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en',
-    debug: true,
-
+    fallbackLng: process.env.REACT_APP_I18N_FALLBACK_LANGUAGE || 'en',
+    debug: process.env.REACT_APP_I18N_DEBUG === 'true' || false,
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     }
   });
 
