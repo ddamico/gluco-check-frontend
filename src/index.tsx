c72ec47af91a63563e00106e58216407c74e9b24
David D'Amico <human@ddamico.org>
2020-10-18 20:56:00 -0400

    moving i18n helper into lib, adding coverage ignore to it

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import './i18n';
+import "./lib/i18n";
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,18 +1,11 @@
+/* istanbul ignore file */
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-// import LanguageDetector from 'i18next-browser-languagedetector';
-
 i18n
-    .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-//   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
+  .use(Backend)
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: process.env.REACT_APP_I18N_FALLBACK_LANGUAGE || 'en',
     debug: process.env.REACT_APP_I18N_DEBUG === 'true' || false,
@@ -20,6 +13,5 @@ i18n
       escapeValue: false,
     }
   });
-
 
 export default i18n;
