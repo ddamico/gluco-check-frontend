ac0fe1fb8eb7ca580a8cf3f153d281448113b514
David D'Amico <human@ddamico.org>
2021-01-04 23:42:49 -0500

    updating warning colour

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 import red from "@material-ui/core/colors/red";
-import deepOrange from "@material-ui/core/colors/deepOrange";
+import orange from "@material-ui/core/colors/orange";
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
@@ -15,7 +15,8 @@ const theme = createMuiTheme({
       main: red.A100,
     },
     warning: {
-      main: deepOrange[500],
+      light: "rgba(245, 124, 0, 0.75)", // orange[700] at 38%, to match material disabled colour
+      main: orange[700],
     },
     background: {
       default: "#fff",
