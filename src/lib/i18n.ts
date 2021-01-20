bb20a7d573894c8fa6ce71eefca85afba9c2ee87
David D'Amico <human@ddamico.org>
2021-01-20 11:04:29 -0500

    reverting translation files name change

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,25 +1,36 @@
 /* istanbul ignore file */
-import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: process.env.REACT_APP_I18N_FALLBACK_LANGUAGE || 'en-US',
+    fallbackLng: process.env.REACT_APP_I18N_FALLBACK_LANGUAGE || "en",
     detection: {
       // this is just the defaults with navigator moved to the front of the line
-      order: ['navigator', 'querystring', 'cookie', 'localStorage', 'sessionStorage', 'htmlTag', 'path', 'subdomain'],
+      order: [
+        "navigator",
+        "querystring",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ],
     },
-    debug: process.env.REACT_APP_I18N_DEBUG === 'true' || false,
+    debug: process.env.REACT_APP_I18N_DEBUG === "true" || false,
     interpolation: {
       escapeValue: false,
     },
   });
 
-i18n.on('languageChanged', (lng) => {document.documentElement.setAttribute('lang', lng);})
+i18n.on("languageChanged", (lng) => {
+  document.documentElement.setAttribute("lang", lng);
+});
 
 export default i18n;
